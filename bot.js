839ef4ac53e348382fbc31c87041623b49df930b
youssef98124 <38530808+youssef98124@users.noreply.github.com>
2018-04-19 18:01:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,16 +3,16 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log('By NightMarE Enjoy');
+  console.log('تعريب يوسف');
 });
 
 client.on('message', message => {
-  if (message.content === '!join') {
+  if (message.content === 'تعال روم') {
       if (message.author.id !== '388463178377330688') return message.react('⚠')
     const channel = message.member.voiceChannel;
 
     channel.join()
-    .then(connection => console.log('Connected!'))
+    .then(connection => console.log('متصل'))
     .catch(console.error);
   }
 });
