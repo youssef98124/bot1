1a7f595cde537e91edb9601603867a8cb9812074
youssef98124 <38530808+youssef98124@users.noreply.github.com>
2018-04-24 03:38:27 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,1 @@
-exports.TOKEN = 'NDE1NjgxMzIyNzYyMzcxMDcy.Db_uqA.ta8Wg-BFKzROqe-97FnbpkxlQJ4'; // ya wr3 la tns5
 
-exports.PREFIX = '';
-
-exports.GOOGLE_API_KEY = 'AIzaSyAdORXg7UZUo7sePv97JyoDqtQVi3Ll0b8';
