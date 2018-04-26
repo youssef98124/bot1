9aba62ca54166936680c866712c95730767d866e
youssef98124 <38530808+youssef98124@users.noreply.github.com>
2018-04-26 22:36:26 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -96,36 +96,7 @@ var unmuteembeddm = new Discord.RichEmbed()
 
 
 
- client.on('message', message => {
-    if (message.content.startsWith("رابط")) {
-        message.channel.createInvite({
-        thing: true,
-        maxUses: 3,
-        maxAge: 3600,
-    }).then(invite =>
-      message.author.sendMessage(invite.url)
-    )
-    const embed = new Discord.RichEmbed()
-        .setColor("RANDOM")
-          .setDescription(" تم أرسال الرابط برسالة خاصة ")
-           .setAuthor(client.user.username, client.user.avatarURL)
-                 .setAuthor(client.user.username, client.user.avatarURL)
-                .setFooter('طلب بواسطة: ' + message.author.tag)
-
-      message.channel.sendEmbed(embed).then(message => {message.delete(10000)})
-              const Embed11 = new Discord.RichEmbed()
-        .setColor("RANDOM")
-        
-    .setDescription(" مدة الرابط : ساعه  عدد استخدامات الرابط : 3 ")
-      message.author.sendEmbed(Embed11)
-    }
-}); 
-
-
-
-
-
-
+ 
 
 client.on('message', message => {
             if(!message.channel.guild) return;
